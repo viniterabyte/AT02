1763d5f0a426d26ea5e71d5c6735b03c77fcd1c9
vinicius rafael <vinirafael49@gmail.com>
2023-08-07 21:04:45 -0300

    Métodos getter e setters implementado

--- AT02.dart
+++ AT02.dart
@@ -1,7 +1,16 @@
 
+import 'dart:ffi';
+
 class Teste {
  var estudante;
   var nota;
+
+  String getestudante() => this.estudante;
+  Double getnota() => this.nota;
+
+  void setestudante(String estu) => this.estudante = estu;
+  void setnota(Double nota) => this.estudante = nota;
+
 
 }
   
